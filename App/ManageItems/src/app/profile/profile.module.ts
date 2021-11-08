0c2d968cb3d2ce7c670a1d60d44db3da422b163b
Rakesh Sahu <rakeshksahu007@gmail.com>
2021-11-08 17:39:30 +0530

    initialize profile manage

--- App/ManageItems/src/app/profile/profile.module.ts
+++ App/ManageItems/src/app/profile/profile.module.ts
@@ -2,20 +2,23 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ManageProfileComponent } from './manage-profile/manage-profile.component';
 import { RouterModule, Routes } from '@angular/router';
+import { UpdateProfileComponent } from './update-profile/update-profile.component';
 
 const routes: Routes = [
   {
     path: '',
     children: [
       { path: '', redirectTo: 'manage' },
-      { path: 'manage', component: ManageProfileComponent }
+      { path: 'manage', component: ManageProfileComponent },
+      { path: 'update', component: UpdateProfileComponent },
     ],
   },
 ];
 
 @NgModule({
   declarations: [
-    ManageProfileComponent
+    ManageProfileComponent,
+    UpdateProfileComponent
   ],
   imports: [
     CommonModule,
